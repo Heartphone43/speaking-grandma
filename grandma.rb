9d060f067d01b46c61169101b0cf96be2d6db57b
brianna <briannaselliott@gmail.com>
2020-10-08 12:27:08 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 */*-
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-*/# unless you shout it (type in all capitals).
+# unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
@@ -16,6 +16,7 @@ if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
-else return "HUH?! SPEAK UP, SONNY!"
+else 
+    return "HUH?! SPEAK UP, SONNY!"
 end
 end
